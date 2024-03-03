4eaa2b1d95bf7c63743e5a7c0811724c47cdcec2
max <68344959+codewmax@users.noreply.github.com>
2024-03-04 00:17:29 +0100

    Remove commented code and fix indentation in background.js

--- main/background.js
+++ main/background.js
@@ -3,7 +3,6 @@
 
 
 // Handle injection
-// Handle injection
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (tab.url && changeInfo.status === 'complete' && tab.url.includes("linkedin") && tab.url.includes("feed")) {
     chrome.tabs.sendMessage(tabId, { request: "active" }, (response) => {
